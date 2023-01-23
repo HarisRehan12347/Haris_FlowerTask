a2af7b19289308aec066fc867d8a3fee9f0f32d4
haris <harisrehan12347@gmail.com>
2023-01-23 14:46:03 +0500

    Flower shop

---
+++ Task/Flowershop/view/frontend/requirejs-config.js
@@ -0,0 +1,9 @@
+var config = {
+    config: {
+        mixins: {
+            'Magento_Checkout/js/action/place-order': {
+                'Task_Flowershop/js/order/flower-order-mixin': true
+            },
+        }
+    }
+};
